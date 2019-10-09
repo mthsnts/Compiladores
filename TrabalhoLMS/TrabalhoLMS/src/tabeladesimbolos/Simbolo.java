7532907c7ac6d56f25589974f8e87904e7791658
Zaphod Beeblebrox <matheussantostb1@gmail.com>
2019-10-09 19:37:38 -0300

    Inicio da tabela de simbolos

---
+++ TrabalhoLMS/TrabalhoLMS/src/tabeladesimbolos/Simbolo.java
@@ -0,0 +1,69 @@
+package tabeladesimbolos;
+
+public class Simbolo {
+
+    private String nome;
+    private String categoria;
+    private int nivel;
+    private int geralA;
+    private int geralB;
+
+    private Simbolo proximo;
+
+    public Simbolo(String nome, String categoria, int nivel, int geralA, int geralB, Simbolo proximo) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.nivel = nivel;
+        this.geralA = geralA;
+        this.geralB = geralB;
+        this.proximo = proximo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getGeralA() {
+        return geralA;
+    }
+
+    public void setGeralA(int geralA) {
+        this.geralA = geralA;
+    }
+
+    public int getGeralB() {
+        return geralB;
+    }
+
+    public void setGeralB(int geralB) {
+        this.geralB = geralB;
+    }
+
+    public Simbolo getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(Simbolo proximo) {
+        this.proximo = proximo;
+    }
+}
---
+++ TrabalhoLMS/TrabalhoLMS/src/tabeladesimbolos/Tabela.java
@@ -0,0 +1,51 @@
+package tabeladesimbolos;
+
+public class Tabela {
+
+    private final int tableSize = 25147; //first prime after 25143
+    int index,collisions;
+    Simbolo[] hashtable = new Simbolo[tableSize];
+
+    public int hash(String key, int tableSize){
+        int hashVal = 0; //uses Horner’s method to evaluate a polynomial
+        for( int i = 0; i < key.length( ); i++ )
+            hashVal = 37 * hashVal + key.charAt( i );
+        hashVal %= tableSize;
+        if( hashVal < 0 )
+            hashVal += tableSize; //needed if hashVal is negative
+        return hashVal;
+    }
+
+    public void adiciona(Simbolo simbolo){
+        index = this.hash(simbolo.getNome(), tableSize);
+        if (isNullIndex(index))
+            this.hashtable[index] = simbolo;
+        else {
+            Simbolo auxSimbolo = this.hashtable[index];
+            do {
+                auxSimbolo = auxSimbolo.getProximo();
+            }while (this.hasProximo(auxSimbolo));
+            auxSimbolo.setProximo(simbolo);
+        }
+    }
+
+    public void remove(String simbolo){
+        index = this.hash(simbolo, tableSize);
+        Simbolo simboloAtual = this.hashtable[index];
+
+        if (simboloAtual.getNome().equals(simbolo)){
+            this.hashtable[index] = simboloAtual.getProximo();
+        }else {
+
+        }
+    }
+
+
+    public boolean isNullIndex(int index){
+        return this.hashtable[index] == null;
+    }
+
+    public boolean hasProximo(Simbolo simbolo){
+        return simbolo.getProximo() != null;
+    }
+}
